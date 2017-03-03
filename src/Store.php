b901903526d42d4ec173b64c74f936c5f23f3a80
Jennifer Beem <jenniferbeem@gmail.com>
2017-03-03 08:49:22 -0800

    add addBrands, find and getBrands methods for Store class

--- src/Store.php
+++ src/Store.php
@@ -43,6 +43,29 @@ class Store
         $GLOBALS['DB']->exec("DELETE FROM brands_stores WHERE brand_id = {$this->getId()};");
     }
 
+    function addBrand($id)
+    {
+        $GLOBALS['DB']->exec("INSERT INTO brands_stores (brand_id, store_id) VALUES ({$id}, {$this->getId()});");
+    }
+
+    function getBrands()
+    {
+        $returned_brands = $GLOBALS['DB']->query("SELECT brands.* FROM stores
+            JOIN brands_authors ON (brands_stores.store_id = stores.id)
+            JOIN brands ON (brands.id = brands_stores.brand_id)
+            WHERE stores.id = {$this->getId()};");
+            if ($returned_brands) {
+                return $returned_brands->fetchAll(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, 'Brand', ['name', 'id']);
+            }
+            return [];
+        }
+
+    static function find($id)
+    {
+        $store = $GLOBALS['DB']->query("SELECT * FROM stores WHERE id={$id}");
+        return $store->fetchAll(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, "Store", ["name"])[0];
+    }
+
     static function getAll()
     {
         $returned_stores = $GLOBALS['DB']->query("SELECT * FROM stores;");
